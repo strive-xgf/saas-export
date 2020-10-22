f36976a4e871b7401feaa3d4425dec5dfe553a94
strive_gf@163.com <strive_gf@163.com>
2020-10-22 19:32:20 +0800

    环境搭建，父工程和子工程的pom.xml都添加<packaging>标签，父工程为pom，web子工程为war，其它子工程都为jar

---
+++ export_system_service/src/test/java/com/xgf/service/company/TestCompanyService.java
@@ -0,0 +1,13 @@
+package com.xgf.service.company;
+
+import com.xgf.domain.company.Company;
+import org.junit.Test;
+
+public class TestCompanyService {
+
+    @Test
+    public void test01(){
+        Company company = new Company();
+        System.out.println(company);
+    }
+}
