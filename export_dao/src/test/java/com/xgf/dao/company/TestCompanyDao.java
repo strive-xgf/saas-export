2bd9d392a5c379b1b7ec6b5d7ffadab6c711e639
strive_gf@163.com <strive_gf@163.com>
2020-10-22 14:25:32 +0800

    SaaS-Export项目环境搭建，父工程子工程创建，父子工程依赖搭建，测试环境搭建情况

---
+++ export_dao/src/test/java/com/xgf/dao/company/TestCompanyDao.java
@@ -0,0 +1,12 @@
+package com.xgf.dao.company;
+
+import com.xgf.domain.company.Company;
+import org.junit.Test;
+
+public class TestCompanyDao {
+    @Test
+    public void test01(){
+        Company company = new Company();
+        System.out.println(company);
+    }
+}
