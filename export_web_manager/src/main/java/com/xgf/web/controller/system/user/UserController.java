19112ff41530bf0a51e2aaf13cbe51d7ea9787cd
strive_gf@163.com <strive_gf@163.com>
2020-11-09 21:49:38 +0800

    shrio的过滤器和标签，apache shiro登录认证功能（拦截非登录用户访问登录模块权限）

--- export_web_manager/src/main/java/com/xgf/web/controller/system/user/UserController.java
+++ export_web_manager/src/main/java/com/xgf/web/controller/system/user/UserController.java
@@ -12,6 +12,11 @@ import com.xgf.service.system.module.IModuleService;
 import com.xgf.service.system.role.IRoleService;
 import com.xgf.service.system.user.IUserService;
 import com.xgf.web.controller.system.BaseController;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -208,6 +214,64 @@ public class UserController extends BaseController {
         session.removeAttribute("loginUser");
         //让session过期，销毁session
         session.invalidate();
+        l.info("loginOut注销用户");
         return "redirect:/login.jsp";//转发不会改地址的数据，只有重定向会
     }
+
+
+    //通过url无权限访问跳转shiro登录，如果session中没有user，访问非放行（过滤器没有anon放行）的页面，就跳转login-shiro进行登录   ${path}/system/user/login-shiro
+    @RequestMapping(path = "/login-shiro", method = {RequestMethod.GET, RequestMethod.POST})
+    public String loginShiro(String email,String password) {
+        //根据 email查询对应的用户
+        l.info("登录输入邮箱 loginShiro email " + email);
+        l.info("登录输入密码 loginShiro password " + password);
+
+        //使用shiro框架进行认证，结果是三种可能（登录成功，密码出错，用户不存在三种情况）
+        // 用户不存在抛出异常 UnknownAccountException，密码出错抛出异常 IncorrectCredentialsException
+        //其实登录login的本质是需要调用realm进行查找用户（subject.login()调用securitymanager，然后调用realm）
+
+        //1. 先获取subject 表示对securitymanager连接
+        Subject subject = SecurityUtils.getSubject();//获取连接
+
+        //2.调用securitymanager
+        //3.再调用realm
+
+        //进行身份验证
+        UsernamePasswordToken token = new UsernamePasswordToken(email, password);
+        try {
+            subject.login(token);//验证正确正确
+            //正确
+            l.info("用户名密码正确");
+            //保存用户信息，要求访问realm返回一个user对象
+            User user = (User) subject.getPrincipal();//访问realm返回一个user对象
+            session.setAttribute("loginUser",user);
+            //查询用户的模块权限，显示到右侧的导航栏，一个 Module对象 就是左侧栏上的一个菜单项
+            List<Module> menus = iModuleService.findModulesByUser(user);
+            session.setAttribute("menus",menus);
+            l.info("登录用户权限模块个数 = "+ menus.size() +"login menus "+menus);
+            //登录成功，跳到主页
+            return "redirect:/home/toMain";
+        } catch (UnknownAccountException e) {//用户不存在，抛出异常UnknownAccountException
+            e.printStackTrace();
+            l.info("用户(邮箱)不存在");
+            request.setAttribute("error","用户（邮箱）不存在");
+            return "forward:/login-shiro.jsp";  //重定向到登录
+        }catch (IncorrectCredentialsException e){//密码出错，抛出异常IncorrectCredentialsException
+            e.printStackTrace();
+            l.info("密码错误");
+            request.setAttribute("error","邮箱或者密码不对，请重新输入");
+            return "forward:/login-shiro.jsp";
+        }
+    }
+
+
+    @RequestMapping(path = "/loginOut-shiro", method = {RequestMethod.GET, RequestMethod.POST})
+    public String loginOutShiro(){
+        //删除session中的用户信息
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        l.info("loginOut-shiro注销用户");
+        return "redirect:/login-shiro.jsp";
+    }
+
 }
--- export_web_manager/src/main/java/com/xgf/web/shiro/AuthRealm.java
+++ export_web_manager/src/main/java/com/xgf/web/shiro/AuthRealm.java
@@ -1,18 +1,23 @@
 package com.xgf.web.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.xgf.domain.system.user.User;
+import com.xgf.service.system.user.IUserService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 //shiro的一个能够查询权限的类
 //不需要再添加@Component,因为在xml中使用bean标签配置了（已经创建了实例）
 //继承授权AuthorizingRealm类，AuthorizingRealm两大功能： 认证（登录账号密码）和 授权（查用户有什么权限）
 public class AuthRealm extends AuthorizingRealm {
+
+    @Autowired
+    IUserService userService;
+
     //日志
     private Logger l = LoggerFactory.getLogger(this.getClass());
     //spring通过反射调用无参构造函数（创建对象）
@@ -22,13 +27,35 @@ public class AuthRealm extends AuthorizingRealm {
     //授权（查用户有什么权限）
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        l.info("AuthRealm doGetAuthorizationInfo 函数执行");
+        l.info("AuthRealm doGetAuthorizationInfo 【授权】函数执行查用户有什么权限）");
         return null;
     }
     //认证（登录账号密码）
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        l.info("AuthRealm doGetAuthenticationInfo 函数执行");
-        return null;
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        l.info("AuthRealm doGetAuthenticationInfo 【认证】函数执行（登录账号密码）");
+
+        //响应login方法和getPrincipal方法的调用 subject.login(token)  （UserController类）
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
+
+        String email = usernamePasswordToken.getUsername();
+        l.info("doGetAuthenticationInfo --" + email);
+        //调service读dao
+        User user = userService.findUserByEmail(email);
+        l.info("doGetAuthenticationInfo --" + user);
+        if (user == null) {
+            //用户不存在
+            return null;//返回null，系统会将null转成UnknownAccountException异常抛出抛出
+        } else {
+            //用户存在的
+            //AuthenticationInfo 返回给 User user = (User) subject.getPrincipal();（UserController类）
+            /**
+               参数1：principal，存放用户登录信息，subject.getPrincipal()获取
+               参数2：数据库的密码
+               参数3：realm的别名，只有在多个Realm的时候才会用，一般不用
+             */
+            AuthenticationInfo info = new SimpleAuthenticationInfo(user, user.getPassword(), "");
+            return info;
+        }
     }
 }
